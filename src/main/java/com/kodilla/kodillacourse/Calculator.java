eb43be7d0e3985acca32acaba19caadc9d12a0a0
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2019-07-28 19:15:27 +0200

    Calculator

---
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.kodillacourse;
+
+public class Calculator {
+    public int add(int a, int b) {
+        int i = a+b;
+        System.out.println("The result is: " + i);
+        return i;
+    }
+
+    public int substract(int a, int b){
+        int i = a - b;
+        System.out.println("The result is: " + i);
+        return i;
+    }
+}
